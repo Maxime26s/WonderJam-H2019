8b00c547c91ea40aa63761a3c6d636d0c722c5f8
Maxime <wixxwistiii@gmail.com>
2019-03-02 13:49:54 -0500

    Pick up Bubbles et Keys

---
+++ Assets/Maxime/Scripts/Items.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public bool used = false;
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (used)
+        {
+            spriteRenderer.color -= new Color(0f, 0f, 0f, 0.33f*Time.deltaTime);
+            if (Time.time - time >= 3)
+                Destroy(this);
+        }
+    }
+}
