62691d12b16fa73fadc8dbba21e761d983f655d7
Charles <renaudcharles14@hotmail.com>
2019-03-03 12:45:43 -0500

    rien

---
+++ Assets/Charles/Clean/Mover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    
+    public float speed;
+    
+    public float amplitude;
+
+    public float multiplier;
+
+    public float oldTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 translateVec = new Vector3(transform.position.x + amplitude, 0.0f, 0.0f)  * speed;
+        transform.Translate(translateVec * Time.deltaTime);
+
+
+       if (Time.time>=oldTime)
+        {
+            speed = -speed;
+            oldTime += 2;
+        }
+
+    }
+     
+}
---
+++ Assets/Charles/Clean/MoverPlanet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPlanet : MonoBehaviour
+{
+
+
+    public float speed;
+
+    public float amplitude;
+
+    public float multiplier;
+
+    public float oldTime;
+
+    public float secs;
+
+    public float jsp;
+
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startPos = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Vector3 translateVec = new Vector3(transform.position.x + amplitude, 0.0f, 0.0f) * speed;
+        transform.Translate (translateVec * Time.deltaTime);
+
+
+        if (Time.time >= oldTime)
+        {
+            Vector3 teleportVec = new Vector3(2000.0f, 0.0f, 0.0f);
+            transform.Translate(teleportVec);
+            oldTime += secs;
+        }
+        */
+
+        float newPos = Mathf.Repeat(Time.time * speed, jsp);
+        transform.position = startPos + Vector2.right * newPos;
+    }
+}
